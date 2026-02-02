eae777208ad36a44f3acd12c08087ff7862dedaa
eedu7 <mueedahmad067@gmail.com>
2026-02-02 13:38:56 +0500

    Updated the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { TanstackReactQuery } from "@/integrations/tanstack-react-query/tanstack-react-query";
+import { TanstackReactQuery } from "@/integrations/tanstack-react-query";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
